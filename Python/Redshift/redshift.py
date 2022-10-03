2e97a23b15d24d751b73cb077a7033b6003fe345
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-10-03 09:18:04 +0100

    refactor: improve design of whole script

--- Python/Redshift/redshift.py
+++ Python/Redshift/redshift.py
@@ -1,52 +1,54 @@
 #!/usr/bin/python3
-'''Redshift gradual que deve ser executado a cada minuto (e.g.: systemd)
+"""Redshift gradual que deve ser executado a cada minuto (e.g.: systemd)
 Também é possível tornar a main function num loop,
-e correr o script como daemon (e.g.: $ setsid ...)'''
+e correr o script como daemon (e.g.: $ setsid ...)"""
 
+import time
 import subprocess
-from datetime import datetime
 
 
-class Redshift:
-    def __init__(self):
-        self.rs_max_value, self.rs_min_value = 8500, 1250
-        self.hour_light_reset = 6
-        self.hour_start_decrease = 12
-        self.hour_stop_decrease = 23
-        self.current_hour = int(datetime.now().strftime("%H"))
-
-    def main(self):
-        if self.hour_light_reset <= self.current_hour \
-                < self.hour_start_decrease:
-            self.rs_value = str(self.rs_max_value)
-            self.run_redshift()
-        elif self.hour_stop_decrease <= self.current_hour \
-                or self.current_hour < self.hour_light_reset:
-            self.rs_value = str(self.rs_min_value)
-            self.run_redshift()
-        else:
-            self.create_function()
-            self.get_redshift_value()
-            self.run_redshift()
-
-    def create_function(self):
-        '''Baseado numa função afim (y = -ax + b), onde x é o tempo decorrido
-        após a hora inicial, e y é o valor a atribuir ao redshift.'''
-        rs_val_delta = int(self.rs_max_value) - int(self.rs_min_value)
-        mins_delta = (int(self.hour_stop_decrease)
-                      - int(self.hour_start_decrease)) * 60
-        self.func_a = rs_val_delta / mins_delta
-
-    def get_redshift_value(self):
-        current_min = int(datetime.now().strftime("%M"))
-        mins_since_hour_start = (self.current_hour
-                                 - self.hour_start_decrease) * 60 + current_min
-        self.rs_value = str(- self.func_a * mins_since_hour_start
-                            + self.rs_max_value)
-
-    def run_redshift(self):
-        subprocess.run(['redshift', '-P', '-O', self.rs_value],
-                       stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+RS_MAX_VAL, RS_MIN_VAL = 8500, 1250
+RESET_LIGHT_HOUR, START_DECREASE_HOUR, STOP_DECREASE_HOUR = 6, 12, 23
+CURRENT_HOUR, CURRENT_MINUTE = map(int, time.strftime("%H %M").split())
 
 
-Redshift().main()
+def main() -> None:
+    if RESET_LIGHT_HOUR <= CURRENT_HOUR < START_DECREASE_HOUR:
+        run_redshift(RS_MAX_VAL)
+    elif STOP_DECREASE_HOUR <= CURRENT_HOUR or RESET_LIGHT_HOUR > CURRENT_HOUR:
+        run_redshift(RS_MIN_VAL)
+    else:
+        run_redshift(CalculateRSVal().main())
+
+
+class CalculateRSVal:
+    """Baseado numa função afim (y = -ax + b), onde x é o tempo decorrido
+    após a hora inicial, e y é o valor a atribuir ao redshift."""
+
+    def main(self) -> int:
+        func_coef = self.get_func_coefficient()
+        minutes_since_hour_start = self.get_minutes_since_hour_start()
+        return self.run_func(func_coef, minutes_since_hour_start)
+
+    @staticmethod
+    def get_func_coefficient() -> float:
+        rs_vals_delta = RS_MAX_VAL - RS_MIN_VAL
+        minutes_delta = (STOP_DECREASE_HOUR - START_DECREASE_HOUR) * 60
+        return rs_vals_delta / minutes_delta
+
+    @staticmethod
+    def get_minutes_since_hour_start() -> float:
+        return (CURRENT_HOUR - START_DECREASE_HOUR) * 60 + CURRENT_MINUTE
+
+    @staticmethod
+    def run_func(func_coef, minutes_since_hour_start) -> int:
+        return int(-func_coef * minutes_since_hour_start + RS_MAX_VAL)
+
+
+def run_redshift(rs_val: int) -> None:
+    cmd = ["redshift", "-P", "-O", str(rs_val)]
+    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+if __name__ == "__main__":
+    main()
