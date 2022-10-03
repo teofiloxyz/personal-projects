748cf4a291e0a63ef5a7030688ccb5d905aacc63
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-10-03 11:19:25 +0100

    refactor: improve design of whole script

--- Python/OCR/OCR.py
+++ Python/OCR/OCR.py
@@ -1,55 +1,36 @@
 #!/usr/bin/python3
+# Select region to screenshot and read it with OCR
 
-import os
-import shutil
+import time
 import subprocess
-from datetime import datetime
-from configparser import ConfigParser
 
 
-class OCRSelection:
-    def __init__(self):
-        self.now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
-        self.tmp_dir = '/tmp/OCR_script'
-        self.img = f'{self.tmp_dir}/img.png'
-
-        self.config = ConfigParser()
-        self.config.read('config.ini')
-        self.lang = self.config['GENERAL']['language']
-        self.terminal = self.config['GENERAL']['terminal']
-        self.txt_path = self.config['GENERAL']['txt_path']
-        self.txt = os.path.join(self.txt_path, f'/OCR_{self.now}')
-
-    def main(self):
-        self.mk_tmp_dir()
-        self.screenshot()
-        self.ocr_img()
-        self.rm_tmp_dir()
-        self.open_txt()
-
-    def mk_tmp_dir(self):
-        if not os.path.exists(self.tmp_dir):
-            os.mkdir(self.tmp_dir)
-
-    def screenshot(self):
-        cmd = f'maim -s {self.img}'
-        subprocess.run(cmd, shell=True)
-
-    def ocr_img(self):
-        cmd = f'tesseract {self.img} {self.txt}'
-        if self.lang is not None:
-            cmd = cmd + f' -l {self.lang}'
-        subprocess.run(cmd, shell=True)
-
-    def rm_tmp_dir(self):
-        if os.path.exists(self.tmp_dir):
-            shutil.rmtree(self.tmp_dir, ignore_errors=True)
-
-    def open_txt(self):
-        cmd = f"notify-send --urgency=low 'Txt at {self.txt}.txt'"
-        subprocess.Popen(cmd, shell=True, start_new_session=True)
-        cmd = f'{self.terminal} -e nvim {self.txt}.txt'
-        subprocess.run(cmd, shell=True)
+NOW = time.strftime("%Y-%m-%d_%H:%M:%S")
+IMG_PATH = f"/tmp/python_OCR_{NOW}.png"
+TXT_PATH = f"/tmp/python_OCR_{NOW}"
+LANGUAGE = None  # "por"/"eng"/... default -> eng
 
 
-OCRSelection().main()
+def main() -> None:
+    take_screenshot()
+    read_img_with_ocr()
+    open_txt_output()
+
+
+def take_screenshot() -> None:
+    subprocess.run(["maim", "--select", IMG_PATH])
+
+
+def read_img_with_ocr() -> None:
+    cmd = f"tesseract {IMG_PATH} {TXT_PATH}"
+    if LANGUAGE is not None:
+        cmd += f" -l {LANGUAGE}"
+    subprocess.run(cmd, shell=True)
+
+
+def open_txt_output() -> None:
+    subprocess.run(["alacritty", "-e", "nvim", f"{TXT_PATH}.txt"])
+
+
+if __name__ == "__main__":
+    main()
