d8a047733a55c5288b2c23ec5fdd7395197f708f
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-11-16 13:29:15 +0000

    refactor: whole module

--- Python/Tfuncs/rofi_integration.py
+++ Python/Tfuncs/rofi_integration.py
@@ -6,42 +6,63 @@ import subprocess
 
 class Rofi:
     @staticmethod
-    def simple_prompt(prompt: str, message="") -> str:
-        if message != "":
-            message = f" -mesg \"{message}\""
-
-        cmd = f"rofi -dmenu -p '{prompt}' -l 0 -theme-str 'entry " \
-              "{ placeholder: \"\"; } inputbar { children: [prompt, " \
-              "textbox-prompt-colon, entry]; } listview { border: 0; }'" \
-              f"{message}"
-        user_input = subprocess.run(cmd, shell=True, capture_output=True) \
-            .stdout.decode('utf-8')
-        return user_input.strip("\n")
-
-    @staticmethod
-    def custom_dmenu(prompt: str, dmenu: list, message="") -> str:
-        if message != "":
-            message = f" -mesg \"{message}\""
-
-        lines = "\n".join(dmenu)
-        input_file = "/tmp/python_rofi_integration.dmenulist"
-        with open(input_file, "w") as rif:
-            rif.writelines(lines)
-
-        if len(dmenu) < 15:
-            dmenu_lines = str(len(dmenu))
-        else:
-            dmenu_lines = "15"
-
-        cmd = f"rofi -dmenu -i -input {input_file} -p '{prompt}'" \
-              f" -l {dmenu_lines} -theme-str 'entry {{ placeholder: \"\"; }} "\
-              "inputbar { children: [prompt, textbox-prompt-colon, entry]; }'"\
-              f"{message}; rm {input_file}"
-        user_input = subprocess.run(cmd, shell=True, capture_output=True) \
-            .stdout.decode('utf-8')
-        return user_input.strip("\n")
+    def simple_prompt(prompt: str, message: str = "") -> str:
+        prompt = fix_prompt(prompt)
+        message = get_prompt_msg(message)
+        cmd = (
+            f"rofi -dmenu -p '{prompt}' -l 0 -theme-str 'entry "
+            '{ placeholder: ""; } inputbar { children: [prompt, '
+            "textbox-prompt-colon, entry]; } listview { border: 0; }'"
+            f"{message}"
+        )
+        return run_and_get_input(cmd)
 
     @staticmethod
-    def message(message: str):
-        cmd = f"rofi -e \"{message}\""
+    def custom_dmenu(prompt: str, dmenu: list, message: str = "") -> str:
+        prompt = fix_prompt(prompt)
+        message = get_prompt_msg(message)
+        input_file = create_input_file(dmenu)
+        dmenu_lines = fix_dmenu_lines_num(len(dmenu))
+        cmd = (
+            f"rofi -dmenu -i -input {input_file} -p '{prompt}'"
+            f' -l {dmenu_lines} -theme-str \'entry {{ placeholder: ""; }} '
+            "inputbar { children: [prompt, textbox-prompt-colon, entry]; }'"
+            f"{message}; rm {input_file}"
+        )
+        return run_and_get_input(cmd)
+
+    @staticmethod
+    def message(message: str) -> None:
+        cmd = f'rofi -e "{message}"'
         subprocess.run(cmd, shell=True)
+
+
+def fix_prompt(prompt: str) -> str:
+    if prompt.endswith(":"):
+        prompt = prompt[:-1]
+    return prompt
+
+
+def get_prompt_msg(message: str) -> str:
+    if message != "":
+        message = f' -mesg "{message}"'
+    return message
+
+
+def create_input_file(dmenu: list) -> str:
+    lines = "\n".join(dmenu)
+    input_file = "/tmp/python_rofi_integration.dmenulist"
+    with open(input_file, "w") as rif:
+        rif.writelines(lines)
+    return input_file
+
+
+def fix_dmenu_lines_num(dmenu_length: int) -> str:
+    return str(dmenu_length) if dmenu_length < 15 else "15"
+
+
+def run_and_get_input(cmd: str) -> str:
+    user_input = subprocess.run(
+        cmd, shell=True, capture_output=True
+    ).stdout.decode("utf-8")
+    return user_input.strip("\n")
