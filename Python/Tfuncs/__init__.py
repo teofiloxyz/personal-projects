70f16ffca1891d6ae95de6d30f6b04defa11902f
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-11-21 14:26:55 +0000

    remove inputs and outputs classes

--- Python/Tfuncs/__init__.py
+++ Python/Tfuncs/__init__.py
@@ -3,16 +3,7 @@
 
 from .generic_menu import menu as gmenu
 from .rofi_integration import Rofi as rofi
-from .generic_input import (Questions as qst,
-                            Inputs as inpt,
-                            Outputs as oupt)
-from .terminal_font import (Fformat as ffmt,
-                            Fcolor as fcol,
-                            Bcolor as bcol)
+from .generic_input import Questions as qst
+from .terminal_font import Fformat as ffmt, Fcolor as fcol, Bcolor as bcol
 
-__all__ = [
-        'generic_menu',
-        'terminal_font',
-        'generic_input',
-        'rofi_integration'
-        ]
+__all__ = ["generic_menu", "terminal_font", "generic_input", "rofi_integration"]
