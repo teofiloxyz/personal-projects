f47963af1adfc8a2afd6a7407e9f42c44e9e52c5
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-12-01 13:53:14 +0000

    fix and refactor class

--- Python/Cleaner/cleaner.py
+++ Python/Cleaner/cleaner.py
@@ -4,41 +4,54 @@ Logs(var(contains journal)); Cleans broken symlinks(except on /run, /proc),
 worthless files/folders and orphaned packages; Run this as root"""
 
 import os
-import shutil
 import subprocess
 from datetime import datetime
 
-
-class Cleaner:
-    def __init__(self):
-        self.today = datetime.now().strftime("%Y-%m-%d")
-        self.arcs_dir = "archives_path"
-
-    def main(self):
-        self.mng_cache()
-        self.mng_history()
-        self.mng_logs()
-        self.mng_symlinks()
-        self.mng_worthless_files()
-        self.mng_pac_orphans()
-
-    def mng_worthless_files(self):
-        files_list = "file_1", "file_2", "file_3"
-        folders_list = "folder_1", "folder_2", "folder_3"
-
-        for file in files_list:
-            if not os.path.isfile(file):
-                continue
-            self.remove_file(file)
-
-        for folder in folders_list:
-            if not os.path.isdir(folder):
-                continue
-            self.remove_folder(folder)
-
-    def mng_pac_orphans(self):
-        cmd = "shell command"
-        subprocess.run(cmd, shell=True)
+from cache import Cache
+from history import History
+from logs import Logs
+from symlinks import Symlinks
+from utils import Utils
 
 
-Cleaner().main()
+TODAY = datetime.now().strftime("%Y-%m-%d")
+ARCS_DIR = "archives_path"
+
+
+def main() -> None:
+    cache, history, logs, symlinks = (
+        Cache(TODAY, ARCS_DIR),
+        History(TODAY, ARCS_DIR),
+        Logs(TODAY, ARCS_DIR),
+        Symlinks(),
+    )
+
+    cache.manage()
+    history.manage()
+    logs.manage()
+    symlinks.manage()
+    mng_worthless_files()
+    mng_orphaned_packs()
+
+
+def mng_worthless_files() -> None:
+    utils = Utils()
+
+    files_list = "file_1", "file_2", "file_3"
+    [utils.remove_file(file) for file in files_list if os.path.isfile(file)]
+
+    folders_list = "folder_1", "folder_2", "folder_3"
+    [
+        utils.remove_folder(folder)
+        for folder in folders_list
+        if os.path.isdir(folder)
+    ]
+
+
+def mng_orphaned_packs() -> None:
+    cmd = "shell command to remove orphaned packages"
+    subprocess.run(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    main()
