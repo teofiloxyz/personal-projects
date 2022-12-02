c5117a971d8c9ad7fc8a52aad32f56be5845a4d0
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-12-02 00:09:51 +0000

    fix and refactor class

--- Python/Cleaner/utils.py
+++ Python/Cleaner/utils.py
@@ -7,46 +7,51 @@ import shutil
 
 class Utils:
     @staticmethod
-    def move_file_or_folder(file_or_folder, destination):
+    def move_file_or_folder(file_or_folder: str, destination: str) -> None:
         shutil.move(file_or_folder, destination)
 
     @staticmethod
-    def copy_file_or_folder(file_or_folder, destination):
+    def copy_file_or_folder(file_or_folder: str, destination: str) -> None:
         if os.path.isdir(file_or_folder):
             shutil.copytree(file_or_folder, destination, dirs_exist_ok=True)
-        else:
-            if os.path.isdir(destination):
-                destination = os.path.join(
-                    destination, os.path.basename(file_or_folder)
-                )
-            shutil.copyfile(file_or_folder, destination)
+            return
+
+        if os.path.isdir(destination):
+            destination = os.path.join(
+                destination, os.path.basename(file_or_folder)
+            )
+        shutil.copyfile(file_or_folder, destination)
 
     @staticmethod
-    def remove_file(file):
+    def remove_file(file: str) -> None:
         os.remove(file)
 
     @staticmethod
-    def remove_folder(folder):
+    def remove_folder(folder: str) -> None:
         shutil.rmtree(folder, ignore_errors=True)
 
     @staticmethod
-    def create_folder(folder):
+    def create_folder(folder: str) -> None:
         os.makedirs(folder, exist_ok=True)
 
     @staticmethod
-    def create_archive(input_dir, output, compress=True):
-        cp = " --use-compress-program='xz -9T0'" if compress is True else ""
+    def create_archive(
+        input_dir: str, output: str, compress: bool = True
+    ) -> None:
+        cp = " --use-compress-program='xz -9T0'" if compress else ""
         subprocess.run(f"tar cf {output}{cp} -C {input_dir} .", shell=True)
 
     @staticmethod
-    def check_dir_size_mb(input_dir):
+    def check_dir_size_mb(input_dir: str) -> int:
         size = subprocess.run(
             ["du", "-m", input_dir], stdout=subprocess.PIPE
         ).stdout.decode("utf-8")
         return int(size.split()[0])
 
     @staticmethod
-    def get_oldest_file(input_dir, extension, reverse=False):
+    def get_oldest_file(
+        input_dir: str, extension: str, reverse: bool = False
+    ) -> str:
         files = {
             os.path.getmtime(os.path.join(input_dir, file)): os.path.join(
                 input_dir, file
@@ -55,6 +60,4 @@ class Utils:
             if file.endswith(extension)
         }
 
-        if reverse is False:
-            return files[min(files.keys())]
-        return files[max(files.keys())]
+        return files[max(files.keys())] if reverse else files[min(files.keys())]
