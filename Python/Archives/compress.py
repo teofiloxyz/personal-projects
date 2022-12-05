e663b7fe90b604e1c945c709e4d9be448bebdaef
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-12-05 18:34:17 +0000

    fix and refactor file

--- Python/Archives/compress.py
+++ Python/Archives/compress.py
@@ -1,151 +1,127 @@
 #!/usr/bin/python3
+# Needs tweaking and testing
+
+import os
+import subprocess
+
+from utils import Utils
 
 
 class Compress:
-    def compress(self):
-        def compress_process(self):
-            get_compression_method(self)
-            check_output_name(self)
-            compress_input(self)
+    def __init__(self) -> None:
+        self.utils = Utils()
+        self.methods_file = {
+            "1": (
+                "xz(max)",
+                ".xz",
+                "xz -9kT0 input --stdout > output",
+            ),
+            "2": (
+                "xz(med)",
+                ".xz",
+                "xz -6kT0 input --stdout > output",
+            ),
+            "3": (
+                "zip",
+                ".zip",
+                "zip --junk-paths output input",
+            ),
+            "4": (
+                "7zip",
+                ".7z",
+                (
+                    "7z a output -mhe=on input -p",
+                    "7z a output input",
+                ),
+            ),
+        }
+        self.methods_folder = {
+            "1": (
+                "tar.xz(max)",
+                ".tar.xz",
+                "tar cvf output --use-compress-program='xz -9T0' input",
+            ),
+            "2": (
+                "tar",
+                ".tar",
+                "tar cvf output input",
+            ),
+            "3": ("zip", ".zip", "zip output input"),
+            "4": (
+                "7zip",
+                ".7z",
+                (
+                    "7z a output -mhe=on input -p",
+                    "7z a output input",
+                ),
+            ),
+            "5": (
+                "tar.xz(med)",
+                ".tar.xz",
+                "tar cvf output --use-compress-program='xz -6T0' input",
+            ),
+        }
 
-        def name_output(self):
-            self.output = oupt.files(
-                question="Enter the output full path, or "
-                "just the name for same input dir, or leave "
-                "empty for same as input\n: ",
-                file_input=self.input,
-                output_name_change=True,
+    def main(self, input_file_or_dir: str) -> None:
+        input_is_entire_folder = self.utils.check_input(input_file_or_dir)
+        if input_is_entire_folder:
+            input_file_or_dir = "."
+        compression_method = self.get_compression_method(input_is_entire_folder)
+        output = self.get_output_name(
+            input_file_or_dir, input_is_entire_folder, compression_method
+        )
+        self.compress_input(input_file_or_dir, output, compression_method)
+
+    def get_compression_method(self, input_is_entire_folder: bool) -> tuple:
+        compression_methods = (
+            self.methods_folder if input_is_entire_folder else self.methods_file
+        )
+
+        prompt = input(
+            "\n".join(
+                f"[{key}] {val[0]}" for key, val in compression_methods.items()
             )
+            + "\nChoose the compression method: "
+        )
+        try:
+            return compression_methods[prompt]
+        except KeyError:
+            self.utils.error("Aborted...")
 
-        def get_compression_method(self):
-            # Ou apenas um ficheiro é comprimido, ou uma pasta inteira
-            if os.path.isfile(self.input):
-                methods = {
-                    "1": (
-                        "xz(max)",
-                        "xz",
-                        f"xz -9kT0 {self.input} "
-                        f"--stdout > {self.output}.xz",
-                    ),
-                    "2": (
-                        "xz(med)",
-                        "xz",
-                        f"xz -6kT0 {self.input} "
-                        f"--stdout > {self.output}.xz",
-                    ),
-                    "3": (
-                        "zip",
-                        "zip",
-                        f"zip --junk-paths {self.output}.zip {self.input}",
-                    ),
-                    "4": (
-                        "7zip",
-                        "7z",
-                        (
-                            f"7z a {self.output}.7z -mhe=on {self.input} -p",
-                            f"7z a {self.output}.7z {self.input}",
-                        ),
-                    ),
-                }
-            else:
-                self.entire_folder = True
-                self.input_folder = self.input
-                input_folder_contents = os.listdir(self.input_folder)
-                self.input = "*"
-
-                # Ver se há algum hidden file na pasta
-                for content in input_folder_contents:
-                    if content[0] == ".":
-                        self.input = "."
-                        break
-
-                if os.path.basename(self.output).split(".")[0] == "":
-                    self.output += os.path.dirname(self.input_folder).split(
-                        "/"
-                    )[-1]
-                methods = {
-                    "1": (
-                        "tar.xz(max)",
-                        "tar.xz",
-                        f"tar cvf {self.output}.tar.xz "
-                        f"--use-compress-program='xz -9T0' {self.input}",
-                    ),
-                    "2": (
-                        "tar.xz(med)",
-                        "tar.xz",
-                        f"tar cvf {self.output}.tar.xz "
-                        f"--use-compress-program='xz -6T0' {self.input}",
-                    ),
-                    "3": ("zip", "zip", f"zip {self.output}.zip {self.input}"),
-                    "4": (
-                        "7zip",
-                        "7z",
-                        (
-                            f"7z a {self.output}.7z -mhe=on {self.input} -p",
-                            f"7z a {self.output}.7z {self.input}",
-                        ),
-                    ),
-                    "5": (
-                        "tar",
-                        "tar",
-                        f"tar cvf {self.output}.tar {self.input}",
-                    ),
-                }
-
-            if self.option == "add":
-                # Caso a opção seja add, o método é detetado
-                self.method = None
-                methods = list(methods.values())
-                for method in methods:
-                    if method[1] == self.extension[1:]:
-                        self.method = method
-                        break
-
-                if self.method is None:
-                    self.error("Archive extension not supported, sorry...")
-            else:
-                question = (
-                    "\n".join(
-                        f"[{key}] {val[0]}" for key, val in methods.items()
-                    )
-                    + "\nChoose compressing method: "
-                )
-                self.method = qst.opts(question, methods)
-                if self.method == "q":
-                    self.error("Aborted...")
-
-        def check_output_name(self):
-            output_name = self.output + "." + self.method[1]
-            while os.path.exists(output_name):
-                output_dirname = os.path.dirname(output_name)
-                output_basename = os.path.basename(output_name)
-                output_name = output_dirname + "_" + output_basename
-
-        def compress_input(self):
-            if self.method[1] == "7z":
-                if input(":: Want encryption? You should! " "[Y/n] ") in (
-                    "",
-                    "y",
-                    "Y",
-                ):
-                    cmd = self.method[2][0]
-                else:
-                    cmd = self.method[2][1]
-            else:
-                cmd = self.method[2]
-
-            if self.entire_folder:
-                current_dir = os.getcwd()
-                os.chdir(self.input_folder)
-                subprocess.run(cmd, shell=True)
-                os.chdir(current_dir)
-            else:
-                subprocess.run(cmd, shell=True)
-
-        if self.option == "add":
-            # self.name vem da função extract
-            self.output = os.path.join(os.getcwd(), self.name + "_new")
-            compress_process(self)
+    def get_output_name(
+        self,
+        input_file_or_dir: str,
+        input_is_entire_folder: bool,
+        compression_method: tuple,
+    ) -> str:
+        extension = compression_method[1]
+        if input_is_entire_folder:
+            output_name = os.path.basename(os.getcwd()) + extension
         else:
-            name_output(self)
-            compress_process(self)
+            output_name = os.path.basename(input_file_or_dir) + extension
+
+        while os.path.isfile(output_name):
+            output_name = "_" + output_name
+        return output_name
+
+    def compress_input(
+        self, input_file_or_dir: str, output: str, compression_method: tuple
+    ) -> None:
+        if compression_method[0] == "7zip":
+            if input(":: Want encryption? You should! [Y/n] ") in (
+                "",
+                "y",
+                "Y",
+            ):
+                cmd = compression_method[2][0]
+            else:
+                cmd = compression_method[2][1]
+        else:
+            cmd = compression_method[2]
+
+        cmd = cmd.replace("input", input_file_or_dir)
+        cmd = cmd.replace("output", output)
+
+        err = subprocess.call(cmd, shell=True)
+        if err != 0:
+            self.utils.error(f"Coudn't compress {input_file_or_dir}")
