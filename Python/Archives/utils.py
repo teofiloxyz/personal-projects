13b2b273ac0691f5db371b1e16adf4d6220f40e7
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-12-05 20:13:22 +0000

    fix and refactor file

--- Python/Archives/utils.py
+++ Python/Archives/utils.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import os
+import subprocess
+
 
 class Utils:
     def error(self, msg):
@@ -7,26 +10,7 @@ class Utils:
         print("Error: " + msg)
         exit(1)
 
-    def check_input(self):
-        try:
-            self.input = sys.argv[2]
-        except IndexError:
-            if self.option == "add":
-                self.error("Command needs an input...")
-            self.input = ""
-
-        self.entire_folder = (
-            True
-            if self.option != "add" and self.input in ("", "all", "a", "al")
-            else False
-        )
-        if not self.entire_folder:
-            self.input = os.path.join(os.getcwd(), self.input)
-            if self.input[-1] == "/":
-                self.input = self.input[:-1]
-        else:
-            self.input = os.getcwd() + "/"
-
-        if not os.path.exists(self.input):
-            self.error(f"'{self.input}' does not exist...")
-        self.basename = os.path.basename(self.input)
+    def check_input(self, input_file_or_dir) -> bool:
+        if os.path.isdir(input_file_or_dir):
+            return True
+        return False
