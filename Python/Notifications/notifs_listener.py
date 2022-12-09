7cfbbf0c5072b71cc86c843bd57a3dc4d7454bb7
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-12-09 19:15:51 +0000

    fix and refactor file

--- Python/Notifications/notifs_listener.py
+++ Python/Notifications/notifs_listener.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Should be triggered every minute
+# Improve main func design
 
 from datetime import datetime
 
@@ -7,31 +8,33 @@ from utils import Utils
 
 
 class NotifsListener:
-    def main(self):
-        if self.check_file() is False:
-            return
-        self.get_data()
-        self.need_update, self.notif_is_mute = False, False
-        while True:
-            if self.check_next_alarm() is False:
-                break
-            self.send_notification()
-            self.need_update, self.notif_is_mute = True, True
-        if self.need_update:
-            self.update_file()
+    def __init__(self) -> None:
+        self.utils = Utils()
+        now = datetime.now().strftime("%Y-%m-%d %H:%M")
+        self.now_strp = datetime.strptime(now, "%Y-%m-%d %H:%M")
 
-    def check_next_alarm(self):
-        if len(self.alarms_list) == 0:
-            return False
-        self.now = datetime.now().strftime("%Y-%m-%d %H:%M")
-        self.now_strp = datetime.strptime(self.now, "%Y-%m-%d %H:%M")
-        self.next_alarm = self.alarms_list[0]
-        self.next_alarm_date = self.next_alarm["date"]
-        self.next_alarm_date_strp = datetime.strptime(
-            self.next_alarm_date, "%Y-%m-%d %H:%M"
-        )
-        if self.now_strp < self.next_alarm_date_strp:
-            return False
-        else:
-            self.notif = self.next_alarm["msg"]
-            self.alarms_list.pop(0)
+    def main(self) -> None:
+        cal_notifs_last_update, notifs = self.utils.Scheduled().load_file()
+        if len(notifs) == 0:
+            return
+
+        file_needs_update, notif_is_muted = False, False
+        for notif in notifs:
+            notif_date = self.get_notification_date(notif)
+            if not self.notification_is_due(notif_date):
+                return
+            self.utils.send_notification(notif["msg"], muted=notif_is_muted)
+            notifs.pop(0)
+            file_needs_update, notif_is_muted = True, True
+
+        if file_needs_update:
+            self.utils.Scheduled().write_file(cal_notifs_last_update, notifs)
+
+    def get_notification_date(self, notif: dict) -> datetime:
+        notif_date = notif["date"]
+        return datetime.strptime(notif_date, "%Y-%m-%d %H:%M")
+
+    def notification_is_due(self, notif_date: datetime) -> bool:
+        if self.now_strp < notif_date:
+            return True
+        return False
