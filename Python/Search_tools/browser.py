11aae032686aafbb2d1d4f53431b821aca10d6d6
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-12-11 22:44:26 +0000

    rename to search_tools

---
+++ Python/Search_tools/browser.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import sys
+import subprocess
+
+
+class Browser:
+    def __init__(self):
+        self.browser = "browser"
+        self.search_engine = "search_engine"
+
+    def main(self, search_type, entry):
+        # Add whatever you want
+        options = {
+            "s": "",
+            "p": "python ",
+            "b": "bash ",
+            "g": "golang ",
+            "la": "latex ",
+            "l": "linux ",
+        }
+        option = options[search_type]
+
+        search_url = option + entry
+        search_url = search_url.replace(" ", "+")
+
+        cmd = (
+            f"{self.browser} --new-tab --url "
+            f"{self.search_engine}{search_url}"
+        )
+        subprocess.Popen(cmd, shell=True, start_new_session=True)
+
+
+if len(sys.argv) > 2:
+    search_type = sys.argv[1]
+    entry = " ".join(sys.argv[2:])
+    Browser().main(search_type, entry)
+else:
+    print("Argument needed...")
---
+++ Python/Search_tools/youtube.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+
+from youtube_search import YoutubeSearch
+
+import sys
+import subprocess
+import time
+
+
+class Youtube:
+    def __init__(self):
+        self.music_playlist = "music_playlist"
+        self.download_path = "download_path"
+        self.max_results = 15
+
+    def main(self, entry):
+        self.entry = entry
+        self.search()
+
+    def search(self):
+        print(f"\nSearching on youtube for '{self.entry}':")
+        results = YoutubeSearch(
+            str(self.entry), max_results=self.max_results
+        ).to_dict()
+
+        self.links = []
+        for n, result in enumerate(results, 1):
+            print(f'[{n}] {result["title"]}')
+            self.links.append(f'https://youtu.be/{result["id"]}')
+
+        while True:
+            self.link_num = input("\nChoose title or search another video: ")
+            try:
+                self.link_num = int(self.link_num)
+                if 1 <= self.link_num <= (len(results)):
+                    self.play()
+                    return
+                else:
+                    print("Wrong number!")
+            except ValueError:
+                if self.link_num == "q":
+                    print("Aborted...")
+                    return
+                self.entry = self.link_num
+                self.search()
+                return
+
+    def play(self):
+        self.link = self.links[int(self.link_num) - 1]
+        while True:
+            mode = input(":: Sound, video or menu (won't close)? [S/v/m] ")
+            if mode == "q":
+                print("Aborted...")
+                return
+            elif mode.lower() in ("", "s"):
+                subprocess.run(["mpv", self.link, "--no-video"])
+                return
+            elif mode.lower() == "v":
+                subprocess.run(["mpv", self.link])
+                return
+            elif mode.lower() == "m":
+                self.menu()
+                return
+
+    def menu(self):
+        print("\nMenu mode")
+        while True:
+            mode = input(":: Sound or video? [S/v] ")
+            if mode == "q":
+                print("Aborted...")
+                return
+            elif mode.lower() in ("", "s"):
+                subprocess.run(["mpv", self.link, "--no-video"])
+                break
+            elif mode.lower() == "v":
+                subprocess.run(["mpv", self.link])
+                break
+
+        menu_ans = input(
+            "\nEnter another search\nOr press <enter> for same\n"
+            "Or [a]dd to playlist\nOr [d]ownload: "
+        )
+        if menu_ans == "a":
+            cmd = f"{self.music_playlist} --add {self.link}"
+            subprocess.run(cmd, shell=True)
+            time.sleep(2)
+
+        elif menu_ans == "d":
+            cmd = (
+                'yt-dlp -f "bestaudio" --continue --no-overwrites '
+                "--ignore-errors --extract-audio "
+                f'-o "{self.download_path}/%(title)s.%(ext)s" {self.link}'
+            )
+            err = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+            if err != 0:
+                print("Error downloading...\nAborting...")
+                return
+            else:
+                print(f"Download at {self.download_path}")
+            time.sleep(2)
+
+        elif menu_ans == "":
+            self.search()
+        else:
+            self.entry = menu_ans
+            self.search()
+
+
+if len(sys.argv) > 1:
+    entry = " ".join(sys.argv[1:])
+    Youtube().main(entry)
+else:
+    print("Argument needed...")
