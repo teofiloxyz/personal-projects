05d3a0d6fa5e03c3ca75ce82c2a22e39de90329d
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-12-13 17:15:20 +0000

    fix and refactor file

--- Python/Search_tools/youtube.py
+++ Python/Search_tools/youtube.py
@@ -1,68 +1,69 @@
 #!/usr/bin/python3
+# still need refactoring!
 
 from youtube_search import YoutubeSearch
 
-import sys
 import subprocess
 import time
 
 
 class Youtube:
-    def __init__(self):
-        self.music_playlist = "music_playlist"
-        self.download_path = "download_path"
-        self.max_results = 15
+    music_playlist = "music_playlist"
+    download_path = "download_path"
+    max_results = 15
 
-    def main(self, entry):
-        self.entry = entry
-        self.search()
+    def main(self, entry: str) -> None:
+        results = self.get_search_results(entry)
+        self.show_results(results)
+        choice_index = self.choose_result(results)
+        if choice_index == 0:
+            return
+        link = self.get_link(results, choice_index)
+        self.play(link)
 
-    def search(self):
-        print(f"\nSearching on youtube for '{self.entry}':")
-        results = YoutubeSearch(
-            str(self.entry), max_results=self.max_results
-        ).to_dict()
+    def get_search_results(self, entry: str) -> list[dict] | str:
+        print(f"\nSearching on youtube for '{entry}':")
+        return YoutubeSearch(entry, max_results=self.max_results).to_dict()
 
-        self.links = []
+    def show_results(self, results: list[dict]) -> None:
         for n, result in enumerate(results, 1):
             print(f'[{n}] {result["title"]}')
-            self.links.append(f'https://youtu.be/{result["id"]}')
 
+    def choose_result(self, results: list) -> int:
         while True:
-            self.link_num = input("\nChoose title or search another video: ")
+            prompt = input("\nChoose title or search another video: ")
             try:
-                self.link_num = int(self.link_num)
-                if 1 <= self.link_num <= (len(results)):
-                    self.play()
-                    return
+                choice = int(prompt)
+                if 1 <= choice <= (len(results)):
+                    return choice
                 else:
                     print("Wrong number!")
             except ValueError:
-                if self.link_num == "q":
+                if prompt == "q":
                     print("Aborted...")
-                    return
-                self.entry = self.link_num
-                self.search()
-                return
+                    return 0
 
-    def play(self):
-        self.link = self.links[int(self.link_num) - 1]
+    def get_link(self, results: list[dict], choice_index: int) -> str:
+        result = results[choice_index - 1]
+        return f'https://youtu.be/{result["id"]}'
+
+    def play(self, link: str) -> None:
         while True:
             mode = input(":: Sound, video or menu (won't close)? [S/v/m] ")
             if mode == "q":
                 print("Aborted...")
                 return
             elif mode.lower() in ("", "s"):
-                subprocess.run(["mpv", self.link, "--no-video"])
+                subprocess.run(["mpv", link, "--no-video"])
                 return
             elif mode.lower() == "v":
-                subprocess.run(["mpv", self.link])
+                subprocess.run(["mpv", link])
                 return
             elif mode.lower() == "m":
-                self.menu()
+                self.menu(link)
                 return
 
-    def menu(self):
+    def menu(self, link: str) -> None:
         print("\nMenu mode")
         while True:
             mode = input(":: Sound or video? [S/v] ")
@@ -70,10 +71,10 @@ class Youtube:
                 print("Aborted...")
                 return
             elif mode.lower() in ("", "s"):
-                subprocess.run(["mpv", self.link, "--no-video"])
+                subprocess.run(["mpv", link, "--no-video"])
                 break
             elif mode.lower() == "v":
-                subprocess.run(["mpv", self.link])
+                subprocess.run(["mpv", link])
                 break
 
         menu_ans = input(
@@ -81,7 +82,7 @@ class Youtube:
             "Or [a]dd to playlist\nOr [d]ownload: "
         )
         if menu_ans == "a":
-            cmd = f"{self.music_playlist} --add {self.link}"
+            cmd = f"{self.music_playlist} --add {link}"
             subprocess.run(cmd, shell=True)
             time.sleep(2)
 
@@ -89,7 +90,7 @@ class Youtube:
             cmd = (
                 'yt-dlp -f "bestaudio" --continue --no-overwrites '
                 "--ignore-errors --extract-audio "
-                f'-o "{self.download_path}/%(title)s.%(ext)s" {self.link}'
+                f'-o "{self.download_path}/%(title)s.%(ext)s" {link}'
             )
             err = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
             if err != 0:
@@ -98,16 +99,3 @@ class Youtube:
             else:
                 print(f"Download at {self.download_path}")
             time.sleep(2)
-
-        elif menu_ans == "":
-            self.search()
-        else:
-            self.entry = menu_ans
-            self.search()
-
-
-if len(sys.argv) > 1:
-    entry = " ".join(sys.argv[1:])
-    Youtube().main(entry)
-else:
-    print("Argument needed...")
