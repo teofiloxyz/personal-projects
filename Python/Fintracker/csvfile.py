c750fae88bc77d2b38bd49a1d72d6926aabd731a
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2022-12-16 17:10:06 +0000

    fix file

--- Python/Fintracker/csvfile.py
+++ Python/Fintracker/csvfile.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 
+from database import Database
+
 
 class CSVFile:
-    def export_to_csv(self):
-        df = pd.read_sql(
-            "SELECT * FROM transactions LEFT JOIN expenses "
-            "USING(transaction_id)",
-            self.db_con,
-        )
-
-        csv_out = oupt.files(
-            question="Enter the path to save the csv file: ",
-            extension="csv",
-            output_name="fintracker_table",
-        )
-        if csv_out == "q":
+    def export_csv(self, db_path: str, db_table: str) -> None:
+        # df = pd.read_sql(
+        #    "SELECT * FROM transactions LEFT JOIN expenses "
+        #    "USING(transaction_id)",
+        #    self.db_con,
+        csv_output = input("Enter csv output path: ")
+        if csv_output == "q":
             print("Aborted...")
             return
 
-        df.to_csv(str(csv_out), encoding="utf-8", index=False)
-
-        if os.path.exists(csv_out):
-            print(f"Export done successfuly\nOutput at '{csv_out}'")
-        else:
-            print("Error, something went wrong exporting to CSV")
+        df = Database.Query(db_path).create_df(db_table)
+        df.to_csv(str(csv_output), encoding="utf-8", index=False)
+        print(f"Export done\nOutput at '{csv_output}'")
