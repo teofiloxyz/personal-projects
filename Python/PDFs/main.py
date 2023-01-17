1ded5105ac65ecc69018c9150fcd3c6344a0cb3f
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2023-01-17 17:54:40 +0000

    fix main file

--- Python/PDFs/main.py
+++ Python/PDFs/main.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
 # Menu PDF com diversas funções
 
-from Tfuncs import gmenu
+from Tfuncs import Menu
 
 from pdf import Pdf
 
 
 def main() -> None:
-    open_menu()
-
-
-def open_menu() -> None:
     pdf = Pdf()
-    title = "PDF-Menu"
-    keys = {
-        "c": (pdf.compress, "compress PDF or folder of PDF's"),
-        "m": (pdf.merge, "merge (concatenate) PDF's"),
-        "s": (pdf.split, "split PDF"),
-        "ec": (pdf.encrypt, "encrypt PDF (AES-256)"),
-        "dc": (pdf.decrypt, "decrypt PDF"),
-        "r": (pdf.rotate, "rotate PDF"),
-        "o": (pdf.ocr, "read PDF with OCR"),
-        "ip": (pdf.convert_from_img, "convert images into PDF"),
-        "pi": (pdf.convert_to_img, "convert PDF into images"),
-        "t": (pdf.change_pdf_title, "change pdf title (not the filename)"),
-    }
-    gmenu(title, keys)
+    menu = Menu(title="PDF-Menu")
+
+    menu.add_option(
+        key="c", func=pdf.compress, help="compress PDF or folder of PDF's"
+    )
+    menu.add_option(key="m", func=pdf.merge, help="merge (concatenate) PDF's")
+    menu.add_option(key="s", func=pdf.split, help="split PDF")
+    menu.add_option(key="ec", func=pdf.encrypt, help="encrypt PDF (AES-256)")
+    menu.add_option(key="dc", func=pdf.decrypt, help="decrypt PDF")
+    menu.add_option(key="r", func=pdf.rotate, help="rotate PDF")
+    menu.add_option(key="o", func=pdf.ocr, help="read PDF with OCR")
+    menu.add_option(
+        key="ip", func=pdf.convert_from_img, help="convert images into PDF"
+    )
+    menu.add_option(
+        key="pi", func=pdf.convert_to_img, help="convert PDF into images"
+    )
+    menu.add_option(
+        key="t",
+        func=pdf.change_pdf_title,
+        help="change pdf title (not the filename)",
+    )
+
+    menu.start()
 
 
 if __name__ == "__main__":
