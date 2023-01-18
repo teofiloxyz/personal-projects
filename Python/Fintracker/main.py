f0a8b30eca59be3f49413fb4ab1b8a5ab7044542
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2023-01-18 19:38:26 +0000

    fix menu

--- Python/Fintracker/main.py
+++ Python/Fintracker/main.py
@@ -4,7 +4,7 @@ onde se registam todas as transações efetuadas.
 Também guarda o balanço, e é possível editá-lo."""
 # Still needs some refactoring
 
-from Tfuncs import gmenu
+from Tfuncs import Menu
 
 from transactions import Transactions
 from balance import Balance
@@ -12,37 +12,48 @@ from charts import Charts
 
 
 def main() -> None:
-    open_menu()
+    transactions, balance, charts = Transactions(), Balance(), Charts()
+    menu = Menu(
+        title="Fintracker-Menu",
+        beginning_func=transactions.show_opening_message,
+    )
 
+    menu.add_option(
+        key="ls",
+        func=lambda timespan=30: transactions.show("all", timespan),
+        help="show past # (default 30) days transactions",
+    )
+    menu.add_option(
+        key="lse",
+        func=lambda timespan=30: transactions.show("expenses", timespan),
+        help="show past # (default 30) days expenses",
+    )
+    menu.add_option(
+        key="lsr",
+        func=lambda timespan=30: transactions.show("revenue", timespan),
+        help="show past # (default 30) days revenue",
+    )
+    menu.add_option(key="lsb", func=balance.show, help="show balance statement")
+    menu.add_option(
+        key="sm", func=transactions.show_summary, help="show summary"
+    )
+    menu.add_option(
+        key="ad", func=transactions.add, help="add transaction to database"
+    )
+    menu.add_option(
+        key="rm",
+        func=transactions.remove,
+        help="remove transaction from database",
+    )
+    menu.add_option(key="ed", func=balance.edit, help="edit balance statement")
+    menu.add_option(key="ch", func=charts.show, help="select and show charts")
+    menu.add_option(
+        key="ex",
+        func=transactions.export_csv,
+        help="export database transactions to CSV file",
+    )
 
-def open_menu() -> None:
-    tra = Transactions()
-    bal = Balance()
-    cht = Charts()
-    title = "Fintracker-Menu"
-    keys = {
-        "ls": (
-            lambda timespan=30: tra.show("all", timespan),
-            "show past # (default 30) days transactions",
-        ),
-        "lse": (
-            lambda timespan=30: tra.show("expenses", timespan),
-            "show past # (default 30) days expenses",
-        ),
-        "lsr": (
-            lambda timespan=30: tra.show("revenue", timespan),
-            "show past # (default 30) days revenue",
-        ),
-        "lsb": (bal.show, "show balance statement"),
-        "sm": (tra.show_summary, "show summary"),
-        "ad": (tra.add, "add transaction to database"),
-        "rm": (tra.remove, "remove transaction from database"),
-        "ed": (bal.edit, "edit balance statement"),
-        "ch": (cht.show, "select and show charts"),
-        "ex": (tra.export_to_csv, "export database transactions to CSV file"),
-    }
-    extra_func = tra.show_opening_message
-    gmenu(title, keys, extra_func)
+    menu.start()
 
 
 if __name__ == "__main__":
