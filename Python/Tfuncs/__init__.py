43c9eccefdc7a38f1a5e9ba7ad119f5f2834a95d
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2023-01-21 19:01:44 +0000

    remove terminal_font and generic_input

--- Python/Tfuncs/__init__.py
+++ Python/Tfuncs/__init__.py
@@ -1,9 +1,7 @@
-# Tfuncs is a custom functions package
+""" Tfuncs is a custom package """
 
 
 from .menu import Menu
-from .rofi_integration import Rofi as rofi
-from .generic_input import Questions as qst
-from .terminal_font import Fformat as ffmt, Fcolor as fcol, Bcolor as bcol
+from .rofi_integration import Rofi
 
-__all__ = ["menu", "terminal_font", "generic_input", "rofi_integration"]
+__all__ = ["menu", "rofi_integration"]
