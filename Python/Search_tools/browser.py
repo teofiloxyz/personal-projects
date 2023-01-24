26b8dc8d69efda91e7877ded8e3ed3968aa5b7fd
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2023-01-24 20:37:02 +0000

    refactor browser & utils

--- Python/Search_tools/browser.py
+++ Python/Search_tools/browser.py
@@ -1,11 +1,9 @@
-import subprocess
-
 from utils import Utils
 
 
 class Browser:
     utils = Utils()
-    browser = "browser"
+    browser = "browser"  # firefox, chrome ...
 
     # Add whatever you want
     options = {
@@ -17,15 +15,18 @@ class Browser:
         "l": "linux ",
     }
 
-    def main(self, cmd_arg: str) -> None:
-        option, entry = self.utils.divide_arg(cmd_arg, self.options)
-        search_url = self.get_search_url(option, entry)
+    def main(self, cmd_query: str) -> None:
+        divided_cmd_query = self.utils.divide_cmd_query(cmd_query, self.options)
+        if not divided_cmd_query:
+            return
+        option, query = divided_cmd_query
+        search_url = self.get_search_url(option, query)
         self.search_on_browser(search_url)
 
-    def get_search_url(self, option: str, entry: str) -> str:
-        search_url = option + entry
+    def get_search_url(self, option: str, query: str) -> str:
+        search_url = option + query
         return search_url.replace(" ", "+")
 
     def search_on_browser(self, search_url) -> None:
         cmd = f"{self.browser} --new-tab --url google.com/{search_url}"
-        subprocess.Popen(cmd, shell=True, start_new_session=True)
+        self.utils.run_cmd_on_new_shell(cmd)
--- Python/Search_tools/utils.py
+++ Python/Search_tools/utils.py
@@ -1,9 +1,20 @@
+import subprocess
+
+
 class Utils:
     @staticmethod
-    def divide_arg(cmd_arg: str, options: dict) -> tuple | None:
-        option, entry = cmd_arg.split("", 1)
+    def divide_cmd_query(cmd_query: str, options: dict) -> tuple | None:
+        option, entry = cmd_query.split("", 1)
         try:
             option = options[option]
         except KeyError:
+            print(
+                "Error with option\n"
+                f"These are the options: {' '.join(options.keys())}"
+            )
             return None
         return option, entry
+
+    @staticmethod
+    def run_cmd_on_new_shell(cmd: str) -> None:
+        subprocess.Popen(cmd, shell=True, start_new_session=True)
