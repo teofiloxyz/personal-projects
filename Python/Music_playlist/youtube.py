fc8cabf54276fc0c59602d060f2f589f23162181
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2023-01-24 17:42:42 +0000

    add YoutubeSearch class

--- Python/Music_playlist/youtube.py
+++ Python/Music_playlist/youtube.py
@@ -1,7 +1,8 @@
-from youtube_search import YoutubeSearch
-
 import os
 import subprocess
+import requests
+import urllib.parse
+import json
 from typing import Optional
 
 
@@ -45,7 +46,7 @@ class Youtube:
             else:
                 ytb_code = entry
             if len(ytb_code) != 11 or " " in ytb_code:
-                search = YoutubeSearch(entry, max_results=1).to_dict()
+                search = YoutubeSearch(entry, max_results=1).main()
                 ytb_code = search[0]["id"]
             return ytb_code
 
@@ -68,3 +69,53 @@ class Youtube:
                 continue
             ytb_code = get_ytb_code(entry)
             self.download(ytb_code, output_dir, custom_title, mp3_output)
+
+
+class YoutubeSearch:  # youtube-search fork
+    def __init__(self, search_query: str, max_results=10) -> None:
+        self.search_query = search_query
+        self.max_results = max_results
+
+    def main(self) -> list:
+        BASE_URL = "https://youtube.com"
+        encoded_search = urllib.parse.quote_plus(self.search_query)
+        url = f"{BASE_URL}/results?search_query={encoded_search}"
+        response = requests.get(url).text
+        while "ytInitialData" not in response:
+            response = requests.get(url).text
+        results = self._parse_html(response)
+        if len(results) > self.max_results:
+            return results[: self.max_results]
+        return results
+
+    def _parse_html(self, response) -> list:
+        results = []
+        start = response.index("ytInitialData") + len("ytInitialData") + 3
+        end = response.index("};", start) + 1
+        json_str = response[start:end]
+        data = json.loads(json_str)
+
+        for contents in data["contents"]["twoColumnSearchResultsRenderer"][
+            "primaryContents"
+        ]["sectionListRenderer"]["contents"]:
+            for video in contents["itemSectionRenderer"]["contents"]:
+                res = {}
+                if "videoRenderer" in video.keys():
+                    video_data = video.get("videoRenderer", {})
+                    res["id"] = video_data.get("videoId", None)
+                    res["title"] = (
+                        video_data.get("title", {})
+                        .get("runs", [[{}]])[0]
+                        .get("text", None)
+                    )
+                    res["duration"] = video_data.get("lengthText", {}).get(
+                        "simpleText", 0
+                    )
+                    res["publish_time"] = video_data.get(
+                        "publishedTimeText", {}
+                    ).get("simpleText", 0)
+                    results.append(res)
+
+            if results:
+                return results
+        return results
