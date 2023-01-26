1eb65fda9c72270d4702b8d138b50b5457e1803b
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2023-01-26 01:35:10 +0000

    remake compress file

--- Python/Archives/compress.py
+++ Python/Archives/compress.py
@@ -1,126 +1,137 @@
-# Needs tweaking and testing
-
-import os
-import subprocess
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
 
 from utils import Utils
 
 
-class Compress:
-    def __init__(self) -> None:
-        self.utils = Utils()
-        self.methods_file = {
-            "1": (
-                "xz(max)",
-                ".xz",
-                "xz -9kT0 input --stdout > output",
-            ),
-            "2": (
-                "xz(med)",
-                ".xz",
-                "xz -6kT0 input --stdout > output",
-            ),
-            "3": (
-                "zip",
-                ".zip",
-                "zip --junk-paths output input",
-            ),
-            "4": (
-                "7zip",
-                ".7z",
-                (
-                    "7z a output -mhe=on input -p",
-                    "7z a output input",
-                ),
-            ),
-        }
-        self.methods_folder = {
-            "1": (
-                "tar.xz(max)",
-                ".tar.xz",
-                "tar cvf output --use-compress-program='xz -9T0' input",
-            ),
-            "2": (
-                "tar",
-                ".tar",
-                "tar cvf output input",
-            ),
-            "3": ("zip", ".zip", "zip output input"),
-            "4": (
-                "7zip",
-                ".7z",
-                (
-                    "7z a output -mhe=on input -p",
-                    "7z a output input",
-                ),
-            ),
-            "5": (
-                "tar.xz(med)",
-                ".tar.xz",
-                "tar cvf output --use-compress-program='xz -6T0' input",
-            ),
-        }
+class CompressionType(Enum):
+    XZ = "XZ (on max compression)"
+    TAR = "Tar"
+    ZIP = "Zip"
+    SEVENZIP = "7zip"
 
-    def main(self, input_file_or_dir: str) -> None:
-        input_is_entire_folder = self.utils.check_input(input_file_or_dir)
-        if input_is_entire_folder:
-            input_file_or_dir = "."
-        compression_method = self.get_compression_method(input_is_entire_folder)
-        output = self.get_output_name(
-            input_file_or_dir, input_is_entire_folder, compression_method
+
+@dataclass
+class CompressionProperties:
+    type: CompressionType
+    cmd: str
+    output_ext: str
+
+
+class CompressionOptions:
+    XZ = CompressionProperties(
+        type=CompressionType.XZ,
+        cmd="tar cvf {output} --use-compress-program='xz -9T0' {input}",
+        output_ext=".tar.xz",
+    )
+    TAR = CompressionProperties(
+        type=CompressionType.TAR,
+        cmd="tar cvf {output} {input}",
+        output_ext=".tar",
+    )
+    ZIP = CompressionProperties(
+        type=CompressionType.ZIP,
+        cmd="zip {output} {input}",
+        output_ext=".zip",
+    )
+    SEVENZIP = CompressionProperties(
+        type=CompressionType.SEVENZIP,
+        cmd="7z a {output} {input}",
+        output_ext=".7z",
+    )
+
+    @classmethod
+    def multiple_files_input(cls) -> tuple:
+        return cls.XZ, cls.TAR, cls.ZIP, cls.SEVENZIP
+
+    @classmethod
+    def single_file_input(cls) -> tuple:
+        cls.XZ.cmd, cls.XZ.output_ext = (
+            "xz -9kT0 {input} --stdout > {output}",
+            ".xz",
         )
-        self.compress_input(input_file_or_dir, output, compression_method)
+        cls.ZIP.cmd = "zip --junk-paths {output} {input}"
+        return cls.XZ, cls.ZIP, cls.SEVENZIP
 
-    def get_compression_method(self, input_is_entire_folder: bool) -> tuple:
-        compression_methods = (
-            self.methods_folder if input_is_entire_folder else self.methods_file
-        )
-
-        prompt = input(
-            "\n".join(
-                f"[{key}] {val[0]}" for key, val in compression_methods.items()
-            )
-            + "\nChoose the compression method: "
-        )
-        try:
-            return compression_methods[prompt]
-        except KeyError:
-            self.utils.error("Aborted...")
-
-    def get_output_name(
-        self,
-        input_file_or_dir: str,
-        input_is_entire_folder: bool,
-        compression_method: tuple,
+    @classmethod
+    def get_command(
+        cls, option: CompressionProperties, input: str, output: str
     ) -> str:
-        extension = compression_method[1]
-        if input_is_entire_folder:
-            output_name = os.path.basename(os.getcwd()) + extension
-        else:
-            output_name = os.path.basename(input_file_or_dir) + extension
+        return option.cmd.format(input=input, output=output)
 
-        while os.path.isfile(output_name):
-            output_name = "_" + output_name
-        return output_name
 
-    def compress_input(
-        self, input_file_or_dir: str, output: str, compression_method: tuple
+class Compress:
+    def __init__(self, input_files: list[str]) -> None:
+        self.utils = Utils()
+        self.input_files = input_files
+
+    def main(self) -> None:
+        self._handle_input()
+
+        compression_options = self._get_compression_options()
+        compression_properties = self._choose_compression_properties(
+            compression_options
+        )
+        if not compression_properties:
+            print("Aborted...")
+            return
+        elif compression_properties.type == CompressionType.SEVENZIP:
+            compression_properties = self._choose_if_7zip_encrypts_output(
+                compression_properties
+            )
+
+        output_name = self._generate_output_file_name(compression_properties)
+        self._run_compression(compression_properties, output_name)
+
+    def _handle_input(self) -> None:
+        if len(self.input_files) > 1:
+            return
+        if self.utils.check_if_is_dir(self.input_files[0]):
+            self.input_files = self.utils.get_dir_contents(self.input_files[0])
+
+    def _get_compression_options(self) -> tuple[CompressionProperties]:
+        if len(self.input_files) > 1:
+            return CompressionOptions.multiple_files_input()
+        return CompressionOptions.single_file_input()
+
+    def _choose_compression_properties(
+        self, compression_options: tuple[CompressionProperties]
+    ) -> Optional[CompressionProperties]:
+        for n, option in enumerate(compression_options, 1):
+            print(f"[{n}] {option.type.value}")
+        prompt = input("\nChoose compression option or leave empty for first: ")
+        if prompt == "":
+            return compression_options[0]
+        elif prompt.isdigit() and 1 <= int(prompt) <= len(compression_options):
+            return compression_options[int(prompt) - 1]
+        return None
+
+    def _choose_if_7zip_encrypts_output(
+        self, compression_properties: CompressionProperties
+    ) -> CompressionProperties:
+        if input(":: Want an encrypted 7zip output? [y/N] ").lower() == "y":
+            compression_properties.cmd = "7z a {output} -mhe=on {input} -p"
+        return compression_properties
+
+    def _generate_output_file_name(
+        self,
+        compression_properties: CompressionProperties,
+    ) -> str:
+        extension = compression_properties.output_ext
+        output_name = (
+            self.utils.get_output_name_in_cwd(self.input_files) + extension
+        )
+        return self.utils.process_output_file_name(output_name)
+
+    def _run_compression(
+        self, compression_properties: CompressionProperties, output_name: str
     ) -> None:
-        if compression_method[0] == "7zip":
-            if input(":: Want encryption? You should! [Y/n] ") in (
-                "",
-                "y",
-                "Y",
-            ):
-                cmd = compression_method[2][0]
-            else:
-                cmd = compression_method[2][1]
-        else:
-            cmd = compression_method[2]
-
-        cmd = cmd.replace("input", input_file_or_dir)
-        cmd = cmd.replace("output", output)
-
-        err = subprocess.call(cmd, shell=True)
+        cmd = CompressionOptions.get_command(
+            compression_properties,
+            input=" ".join(self.input_files),
+            output=output_name,
+        )
+        err = self.utils.run_cmd(cmd)
         if err != 0:
-            self.utils.error(f"Coudn't compress {input_file_or_dir}")
+            print("Error compressing input")
--- Python/Archives/utils.py
+++ Python/Archives/utils.py
@@ -3,12 +3,43 @@ import subprocess
 
 
 class Utils:
-    def error(self, msg):
-        subprocess.Popen(["paplay", "rejected.wav"], start_new_session=True)
-        print("Error: " + msg)
-        exit(1)
+    @staticmethod
+    def run_cmd(cmd: str) -> int:
+        return subprocess.call(cmd, shell=True)
 
-    def check_input(self, input_file_or_dir) -> bool:
-        if os.path.isdir(input_file_or_dir):
+    @staticmethod
+    def check_if_is_dir(path: str) -> bool:
+        if os.path.isdir(path):
             return True
         return False
+
+    @staticmethod
+    def get_output_name_in_cwd(input_files: list) -> str:
+        cwd = os.getcwd()
+        if len(input_files) == 1:
+            basename = os.path.basename(input_files[0])
+            return os.path.join(cwd, basename)
+        return os.path.join(cwd, "Compressed")
+
+    @staticmethod
+    def get_dir_contents(dir_path: str) -> list[str]:
+        return [os.path.join(dir_path, file) for file in os.listdir(dir_path)]
+
+    @staticmethod
+    def process_output_file_name(output_file: str) -> str:
+        if os.path.exists(output_file):
+            i = 1
+            output_file_noext, file_ext = os.path.splitext(output_file)
+            while os.path.exists(f"{output_file_noext}_{i}{file_ext}"):
+                i += 1
+            output_file = f"{output_file_noext}_{i}{file_ext}"
+        return output_file
+
+    @staticmethod
+    def process_output_dir_name(output_dir: str) -> str:
+        if os.path.exists(output_dir):
+            i = 1
+            while os.path.exists(f"{output_dir}_{i}"):
+                i += 1
+            output_dir = f"{output_dir}_{i}"
+        return output_dir
