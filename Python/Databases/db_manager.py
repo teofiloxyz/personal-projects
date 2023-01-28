562cccd7c14ac73cb13597160c341af0afa61b42
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2023-01-28 17:57:51 +0000

    fix DBManager

--- Python/Databases/db_manager.py
+++ Python/Databases/db_manager.py
@@ -1,7 +1,6 @@
 import os
-import subprocess
 
-from database import Database
+from database import Query as db_query, Edit as db_edit
 
 
 class DBManager:
@@ -16,7 +15,7 @@ class DBManager:
         ]
 
     def get_database_tabs(self, db_path: str) -> list[str]:
-        return Database.Query(db_path).get_tabs()
+        return db_query(db_path).get_tabs()
 
     def choose_database(self) -> tuple[str, str]:
         databases = self.get_databases()
@@ -64,11 +63,8 @@ class DBManager:
         if db_tab == "q":
             return
 
-        print(
-            f"{ffmt.bold}{fcol.green}{db_tab.capitalize()} from "
-            f"{db_name.capitalize()}:{ffmt.reset}"
-        )
-        df = Database.Query(db_path).create_df(db_tab)
+        print(f"{db_tab.capitalize()} from {db_name.capitalize()}:")
+        df = db_query(db_path).create_df(db_tab)
         print(df)
 
     def add_entry_to_db_tab(self) -> None:
@@ -80,7 +76,7 @@ class DBManager:
         if db_tab == "q":
             return
 
-        columns = Database.Query(db_path).get_tab_columns(db_tab)
+        columns = db_query(db_path).get_tab_columns(db_tab)
         entry = []
         for column in columns:
             column_name = column[1]
@@ -95,7 +91,7 @@ class DBManager:
                 column_entry = float(column_entry)
             entry.append(column_entry)
 
-        Database.Edit(db_path).add_entry(db_tab, tuple(entry))
+        db_edit(db_path).add_entry(db_tab, tuple(entry))
 
     def remove_entry_from_db_tab(self) -> None:
         db_name, db_path = self.choose_database()
@@ -106,11 +102,8 @@ class DBManager:
         if db_tab == "q":
             return
 
-        print(
-            f"{ffmt.bold}{fcol.green}{db_tab.capitalize()} from "
-            f"{db_name.capitalize()}:{ffmt.reset}"
-        )
-        df = Database.Query(db_path).create_df(db_tab)
+        print(f"{db_tab.capitalize()} from {db_name.capitalize()}:")
+        df = db_query(db_path).create_df(db_tab)
         print(df)
 
         while True:
@@ -138,15 +131,15 @@ class DBManager:
                     continue
             break
 
-        [Database.Edit(db_path).remove_entry(db_tab, rowid) for rowid in rowids]
+        [db_edit(db_path).remove_entry(db_tab, rowid) for rowid in rowids]
 
     def db_tab_to_csv(self) -> None:
         _, db_path = self.choose_database()
         if db_path == "q":
             return
 
-        db_tab = self.choose_database_tab(db_path)
-        if db_tab == "q":
+        db_table = self.choose_database_tab(db_path)
+        if db_table == "q":
             return
 
         csv_output = input("Enter csv output path: ")
@@ -154,7 +147,7 @@ class DBManager:
             print("Aborted...")
             return
 
-        df = Database.Query(db_path).create_df(db_table)
+        df = db_query(db_path).create_df(db_table)
         df.to_csv(str(csv_output), encoding="utf-8", index=False)
         print(f"Export done\nOutput at '{csv_output}'")
 
@@ -194,7 +187,7 @@ class DBManager:
                 break
 
         db_tab_name = prompt
-        Database.Edit(db_path).create_tab_from_csv(csv_input, db_tab_name)
+        db_edit(db_path).create_tab_from_csv(csv_input, db_tab_name)
 
     def create_db_tab(self) -> None:
         _, db_path = self.choose_database()
@@ -267,7 +260,7 @@ class DBManager:
                 columns_name_list.append((column_name, column_type))
 
         columns = ", ".join(f"{x[0]} {x[1]}" for x in columns_name_list)
-        Database.Edit(db_path).create_tab(db_tab, columns)
+        db_edit(db_path).create_tab(db_tab, columns)
 
     def remove_db_tab(self) -> None:
         db_name, db_path = self.choose_database()
@@ -285,7 +278,7 @@ class DBManager:
             )
             == "y"
         ):
-            Database.Edit(db_path).remove_tab(db_tab)
+            db_edit(db_path).remove_tab(db_tab)
 
     def create_db(self) -> None:
         databases = self.get_databases()
@@ -305,7 +298,7 @@ class DBManager:
                 continue
             break
 
-        subprocess.run(["touch", new_db_path])
+        open(new_db_path, "x")
         if os.path.exists(new_db_path):
             print(f"Database created successfuly at '{new_db_path}'")
         else:
