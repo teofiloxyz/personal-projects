089f8fb1323d87a9604f7e599bd93dc9b8f7923f
teofiloxyz <105755958+teofiloxyz@users.noreply.github.com>
2023-01-28 17:15:54 +0000

    remake database and csvfile

--- Python/Databases/database.py
+++ Python/Databases/database.py
@@ -1,70 +1,73 @@
-#!/usr/bin/python3
-
 import pandas as pd
 
 import sqlite3
 
 
 class Database:
-    def connect(
-        self, db_path: str
-    ) -> tuple[sqlite3.Connection, sqlite3.Cursor]:
-        self.db_con = sqlite3.connect(db_path)
+    def __init__(self, db_path: str) -> None:
+        self.db_path = db_path
+
+    def __enter__(self) -> tuple[sqlite3.Connection, sqlite3.Cursor]:
+        self.db_con = sqlite3.connect(self.db_path)
         self.cursor = self.db_con.cursor()
         return self.db_con, self.cursor
 
-    def disconnect(self) -> None:
+    def __exit__(self, *args) -> None:
         self.db_con.close()
 
-    class Query:
-        def __init__(self, db_path: str) -> None:
-            self.db = Database()
-            self.db_con, self.cursor = self.db.connect(db_path)
 
-        def get_tabs(self) -> list[str]:
-            self.cursor.execute(
+class Query:
+    def __init__(self, db_path: str) -> None:
+        self.db = Database(db_path)
+
+    def get_tabs(self) -> list[str]:
+        with self.db as (_, db_cursor):
+            db_cursor.execute(
                 "SELECT name FROM sqlite_master WHERE type='table'"
             )
-            tabs = self.cursor.fetchall()
-            self.db.disconnect()
-            return tabs
+        return db_cursor.fetchall()
 
-        def create_df(self, db_table: str) -> pd.DataFrame:
-            df = pd.read_sql(f"SELECT * FROM {db_table}", self.db_con)
-            self.db.disconnect()
-            return df
+    def create_df(self, db_table: str) -> pd.DataFrame:
+        with self.db as (db_con, _):
+            return pd.read_sql(f"SELECT * FROM {db_table}", db_con)
 
-        def get_tab_columns(self, db_table: str) -> list[str]:
-            self.cursor.execute(f"pragma table_info({db_table})")
-            columns = self.cursor.fetchall()
-            self.db.disconnect()
-            return columns
+    def export_tab_to_csv(self, csv_output: str, db_table: str) -> None:
+        with self.db as (db_con, _):
+            df = pd.read_sql(f"SELECT * FROM {db_table}", db_con)
+            df.to_csv(csv_output, encoding="utf-8", index=False)
 
-    class Edit:
-        def __init__(self, db_path: str) -> None:
-            self.db = Database()
-            self.db_con, self.cursor = self.db.connect(db_path)
+    def get_tab_columns(self, db_table: str) -> list[str]:
+        with self.db as (_, db_cursor):
+            db_cursor.execute(f"pragma table_info({db_table})")
+            return db_cursor.fetchall()
 
-        def add_entry(self, table: str, entry: tuple) -> None:
-            self.cursor.execute(f"INSERT INTO {table} VALUES {entry}")
-            self.db_con.commit()
-            self.db.disconnect()
 
-        def remove_entry(self, table: str, rowid: str) -> None:
-            self.cursor.execute(f"DELETE FROM {table} WHERE rowid={rowid}")
-            self.db_con.commit()
-            self.db.disconnect()
+class Edit:
+    def __init__(self, db_path: str) -> None:
+        self.db = Database(db_path)
 
-        def create_tab(self, table: str, columns: str) -> None:
-            self.cursor.execute(f"CREATE TABLE {table} ({columns})")
-            self.db_con.commit()
-            self.db.disconnect()
+    def add_entry(self, table: str, entry: tuple) -> None:
+        db_cmd = f"INSERT INTO {table} VALUES {entry}"
+        self._execute(db_cmd)
 
-        def remove_tab(self, table: str) -> None:
-            self.cursor.execute(f"DROP TABLE {table}")
-            self.db_con.commit()
-            self.db.disconnect()
+    def remove_entry(self, table: str, rowid: str) -> None:
+        db_cmd = f"DELETE FROM {table} WHERE rowid={rowid}"
+        self._execute(db_cmd)
 
-        def create_tab_from_csv(self, csv_input: str, tab_name: str) -> None:
+    def create_tab(self, table: str, columns: str) -> None:
+        db_cmd = f"CREATE TABLE {table} ({columns})"
+        self._execute(db_cmd)
+
+    def remove_tab(self, table: str) -> None:
+        db_cmd = f"DROP TABLE {table}"
+        self._execute(db_cmd)
+
+    def create_tab_from_csv(self, csv_input: str, tab_name: str) -> None:
+        with self.db as (db_con, _):
             df = pd.read_csv(csv_input)
-            df.to_sql(tab_name, self.db_con, index=False)
+            df.to_sql(tab_name, db_con, index=False)
+
+    def _execute(self, db_cmd: str) -> None:
+        with self.db as (db_con, db_cursor):
+            db_cursor.execute(db_cmd)
+            db_con.commit()
--- Python/Databases/menu.py
+++ Python/Databases/menu.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 from Tfuncs import ffmt, fcol
 
 import os
@@ -154,7 +152,14 @@ class Menu:
         if db_tab == "q":
             return
 
-        CSVFile().export_csv(db_path, db_tab)
+        csv_output = input("Enter csv output path: ")
+        if csv_output == "q":
+            print("Aborted...")
+            return
+
+        df = Database.Query(db_path).create_df(db_table)
+        df.to_csv(str(csv_output), encoding="utf-8", index=False)
+        print(f"Export done\nOutput at '{csv_output}'")
 
     def csv_to_db_tab(self) -> None:
         _, db_path = self.choose_database()
@@ -163,7 +168,36 @@ class Menu:
 
         db_tabs = self.get_database_tabs(db_path)
 
-        CSVFile().import_csv(db_path, db_tabs)
+        csv_input = input("Enter csv input path: ")
+        if csv_input == "q":
+            print("Aborted...")
+            return
+
+        csv_name = os.path.basename(csv_input)
+        db_tab_name = os.path.splitext(csv_name)[0]
+
+        while True:
+            prompt = input(
+                f"Enter the name for the table, or leave empty to "
+                f"name it '{db_tab_name}': "
+            )
+            if prompt == "q":
+                print("Aborted...")
+                return
+            elif prompt == "":
+                break
+            elif len(prompt) > 30 or " " in prompt:
+                print("Invalid name, might be too big")
+                continue
+
+            if prompt in db_tabs:
+                print(f"There is already a table named '{prompt}'")
+                continue
+            else:
+                break
+
+        db_tab_name = prompt
+        Database.Edit(db_path).create_tab_from_csv(csv_input, db_tab_name)
 
     def create_db_tab(self) -> None:
         _, db_path = self.choose_database()
